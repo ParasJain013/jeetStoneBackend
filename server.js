d3218ce000dbe0397fe81ff057efca8b3679a5da
Paras Jain <64364245+ParasJain013@users.noreply.github.com>
2025-10-03 23:48:32 +0530

    test change

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 require('dotenv').config();
 
 const app = express();
-
+app.use((req,res)=>console.log(JSON.stringify(req)));
 app.use(cors({
   origin: ["http://localhost:5173","https://faguna-stones.vercel.app", "https://www.jeetstoneindustries.in", "https://api.cron-job.org"],
   credentials: true
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use('/api/admin', adminRoutes);
 app.use('/api/app', appRoutes);
 app.use('/api/contact', contactRoutes);
-app.get('/', (req, res) => {
+app.get('/cron', (req, res) => {
     res.send('API is running...');
 });
 
